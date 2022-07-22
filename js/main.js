66f2b9af7ed8668d36d52bc56e5192d6fe3275eb
Matias2302 <matias.23.02@gmail.com>
2022-07-22 17:27:54 -0300

    modales casi cerrados

--- js/main.js
+++ js/main.js
@@ -24,6 +24,16 @@ var btn = document.getElementsByClassName("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close");
 
+// global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('video');
+  
+}
+
 
 // When the user clicks the button, open the modal 
 btn[0].onclick = function() {
@@ -39,7 +49,6 @@ btn[2].onclick = function() {
 //Modales de videos testimonios
 btn[3].onclick = function() {
     modal[3].style.display = "block";
-    console.log(modal);
 }
 btn[4].onclick = function() {
     modal[4].style.display = "block";
@@ -60,32 +69,41 @@ btn[8].onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span[0].onclick = function() {
-    modal[0].style.display = "none";
-    console.log(span);
+    modal[0].style.display = "none";    
+    document.getElementById("player-video").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 span[1].onclick = function() {
     modal[1].style.display = "none";
+    document.getElementById("player-video2").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 span[2].onclick = function() {
     modal[2].style.display = "none";
+    document.getElementById("player-video3").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 span[3].onclick = function() {
     modal[3].style.display = "none";
+    document.getElementById("player-video4").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 span[4].onclick = function() {
     modal[4].style.display = "none";
+    document.getElementById("player-video5").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 span[5].onclick = function() {
     modal[5].style.display = "none";
+    document.getElementById("player-video6").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+    console.log("hola");
 }
 span[6].onclick = function() {
     modal[6].style.display = "none";
+    document.getElementById("player-video7").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 span[7].onclick = function() {
     modal[7].style.display = "none";
+    document.getElementById("player-video8").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 span[8].onclick = function() {
     modal[8].style.display = "none";
+    document.getElementById("player-video9").contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
 }
 
 // When the user clicks anywhere outside of the modal, close it
